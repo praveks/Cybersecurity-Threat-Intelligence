8e31407633bd849707d759204d55fc4a3c9e0cb2
praveks <ksp281097@gmail.com>
2025-04-21 00:39:42 +0530

    frontend bug fixed

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ThreatDashboard from './components/ThreatDashboard';
-import './App.css';
 import Login from './components/login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from './components/signin';
--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap here
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
