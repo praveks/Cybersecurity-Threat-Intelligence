807e08fc17368e465f041af368a9543650f262e4
praveks <ksp281097@gmail.com>
2025-04-20 17:05:55 +0530

    docker changes

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This allows the container to expose the server to your local machine
+    port: 5173,  // Default port for Vite inside the container
+  },
 })
